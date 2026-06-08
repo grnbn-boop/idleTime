72c5eb7aa20e6718f0596b6fb637200f6ce37162
grnbn-boop <vincent_marchesano@hotmail.com>
2026-06-08 14:47:55 -0400

    Clean up player movement and animations
    Switched to RigidBody2D to deal with terrain. Player is now properly animating when jumping and falling. Still needs some timing polish but looking better.

--- Assets/IdleGame/Scripts/Player/ClickToMove2D.cs
+++ Assets/IdleGame/Scripts/Player/ClickToMove2D.cs
@@ -1,37 +1,109 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 namespace IdleTime.Player
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public sealed class ClickToMove2D : MonoBehaviour
     {
         [SerializeField] private float moveSpeed = 4f;
         [SerializeField] private float stopDistance = 0.05f;
+        [SerializeField] private float jumpHeight = 1.5f;
+        [SerializeField] private float gravity = 18f;
+        [SerializeField] private LayerMask terrainMask = ~0;
+        [SerializeField] private Vector2 groundSensorOffset = new Vector2(0f, -0.66f);
+        [SerializeField] private Vector2 groundSensorSize = new Vector2(0.4f, 0.08f);
+        [SerializeField] private float groundCheckDistance = 0.08f;
+        [SerializeField] private Vector2 wallSensorOffset = new Vector2(0.46f, -0.3f);
+        [SerializeField] private Vector2 wallSensorSize = new Vector2(0.08f, 0.56f);
+        [SerializeField] private float wallCheckDistance = 0.08f;
+        [SerializeField] private float wallJumpPreparationDuration = 0.12f;
+        [SerializeField] private float wallJumpHorizontalDelay = 0.08f;
+        [SerializeField] private float groundClickProjectionHeight = 4f;
+        [SerializeField] private float landingPulseDuration = 0.08f;
+        [SerializeField] private float landingLagDuration = 0.18f;
+        [SerializeField] private Tilemap groundTilemap;
         [SerializeField] private Camera worldCamera;
         [SerializeField] private bool flipSpriteToDirection = true;
 
         private SpriteRenderer spriteRenderer;
+        private Rigidbody2D body;
+        private Animator animator;
+
+        private readonly RaycastHit2D[] castHits = new RaycastHit2D[8];
+        private ContactFilter2D terrainFilter;
+
         private float targetX;
         private bool hasTarget;
-
-        private Animator animator;
+        private float verticalVelocity;
+        private bool isGrounded;
+        private bool landedThisFrame;
+        private float landingPulseTimer;
+        private float landingLagTimer;
+        private bool isPreparingWallJump;
+        private float wallJumpPreparationTimer;
+        private float wallJumpHorizontalDelayTimer;
+        private float horizontalVelocity;
 
         private void Awake()
         {
             spriteRenderer = GetComponent<SpriteRenderer>();
-            targetX = transform.position.x;
+            body = GetComponent<Rigidbody2D>();
             animator = GetComponent<Animator>();
+            targetX = transform.position.x;
+
+            if (body == null)
+            {
+                body = gameObject.AddComponent<Rigidbody2D>();
+            }
+
+            body.bodyType = RigidbodyType2D.Kinematic;
+            body.gravityScale = 0f;
+            body.freezeRotation = true;
+
+            terrainFilter = new ContactFilter2D
+            {
+                useLayerMask = true,
+                useTriggers = false
+            };
+            terrainFilter.SetLayerMask(terrainMask);
 
             if (worldCamera == null)
             {
                 worldCamera = Camera.main;
+            }
+
+            if (groundTilemap == null)
+            {
+                groundTilemap = FindFirstTerrainTilemap();
             }
         }
 
         private void Update()
         {
             ReadClickTarget();
-            MoveTowardTarget();
+        }
+
+        private void FixedUpdate()
+        {
+            landedThisFrame = false;
+            Vector2 position = body.position;
+
+            isGrounded = IsTouchingGround(position);
+            if (isGrounded && verticalVelocity < 0f)
+            {
+                verticalVelocity = 0f;
+            }
+
+            float horizontalMove = GetHorizontalMove(position, Time.fixedDeltaTime);
+            float verticalMove = GetVerticalMove(ref position, Time.fixedDeltaTime);
+
+            position.x += horizontalMove;
+            position.y += verticalMove;
+
+            body.MovePosition(position);
+            UpdateAnimation();
         }
 
         private void ReadClickTarget()
@@ -49,7 +121,13 @@ namespace IdleTime.Player
 
             Vector2 screenPosition = Mouse.current.position.ReadValue();
             Vector3 worldPosition = cameraToUse.ScreenToWorldPoint(new Vector3(screenPosition.x, screenPosition.y, -cameraToUse.transform.position.z));
-            targetX = worldPosition.x;
+
+            if (!TryGetClickedGround(worldPosition, out Vector2 groundPoint))
+            {
+                return;
+            }
+
+            targetX = groundPoint.x;
             hasTarget = true;
 
             if (flipSpriteToDirection && spriteRenderer != null)
@@ -58,32 +136,317 @@ namespace IdleTime.Player
             }
         }
 
-        private void MoveTowardTarget()
+        private float GetHorizontalMove(Vector2 position, float deltaTime)
         {
+            horizontalVelocity = 0f;
             if (!hasTarget)
+            {
+                isPreparingWallJump = false;
+                return 0f;
+            }
+
+            float remainingDistance = targetX - position.x;
+            if (Mathf.Abs(remainingDistance) <= stopDistance)
+            {
+                hasTarget = false;
+                return 0f;
+            }
+
+            float direction = Mathf.Sign(remainingDistance);
+            if (landingLagTimer > 0f)
+            {
+                landingLagTimer = Mathf.Max(0f, landingLagTimer - deltaTime);
+                return 0f;
+            }
+
+            if (isPreparingWallJump)
+            {
+                wallJumpPreparationTimer = Mathf.Max(0f, wallJumpPreparationTimer - deltaTime);
+                if (wallJumpPreparationTimer > 0f)
+                {
+                    return 0f;
+                }
+
+                isPreparingWallJump = false;
+                StartJump();
+                wallJumpHorizontalDelayTimer = wallJumpHorizontalDelay;
+                return 0f;
+            }
+
+            if (wallJumpHorizontalDelayTimer > 0f)
+            {
+                wallJumpHorizontalDelayTimer = Mathf.Max(0f, wallJumpHorizontalDelayTimer - deltaTime);
+                return 0f;
+            }
+
+            if (IsWallAhead(position, direction))
+            {
+                if (isGrounded)
+                {
+                    isPreparingWallJump = true;
+                    wallJumpPreparationTimer = wallJumpPreparationDuration;
+                    return 0f;
+                }
+
+                if (verticalVelocity <= 0f)
+                {
+                    return 0f;
+                }
+            }
+
+            float moveDistance = Mathf.Min(Mathf.Abs(remainingDistance), moveSpeed * deltaTime);
+            horizontalVelocity = direction * moveDistance / deltaTime;
+            return direction * moveDistance;
+        }
+
+        private float GetVerticalMove(ref Vector2 position, float deltaTime)
+        {
+            bool wasGrounded = isGrounded;
+            verticalVelocity -= gravity * deltaTime;
+            float requestedMove = verticalVelocity * deltaTime;
+
+            if (requestedMove <= 0f && TryGetGroundBelow(position, Mathf.Abs(requestedMove) + groundCheckDistance, out RaycastHit2D groundHit))
+            {
+                float feetY = position.y + groundSensorOffset.y - groundSensorSize.y * 0.5f;
+                float distanceToGround = feetY - groundHit.point.y;
+                float allowedFall = Mathf.Max(0f, distanceToGround);
+                requestedMove = -Mathf.Min(Mathf.Abs(requestedMove), allowedFall);
+                isGrounded = true;
+                verticalVelocity = 0f;
+            }
+            else
+            {
+                isGrounded = false;
+            }
+
+            if (!wasGrounded && isGrounded)
+            {
+                landedThisFrame = true;
+                landingPulseTimer = landingPulseDuration;
+                landingLagTimer = landingLagDuration;
+            }
+
+            return requestedMove;
+        }
+
+        private bool TryGetClickedGround(Vector2 clickWorldPosition, out Vector2 groundPoint)
+        {
+            groundPoint = Vector2.zero;
+
+            if (!TryGetClickedTile(clickWorldPosition, out Tilemap clickedTilemap, out Vector3Int clickedCell))
+            {
+                return false;
+            }
+
+            Vector2 rayOrigin = clickWorldPosition + Vector2.up * groundClickProjectionHeight;
+            float rayDistance = groundClickProjectionHeight * 2f;
+            int hitCount = Physics2D.Raycast(rayOrigin, Vector2.down, terrainFilter, castHits, rayDistance);
+            for (int i = 0; i < hitCount; i++)
+            {
+                if (IsOwnCollider(castHits[i].collider))
+                {
+                    continue;
+                }
+
+                groundPoint = castHits[i].point;
+                return true;
+            }
+
+            groundPoint = GetTileTopPoint(clickedTilemap, clickedCell, clickWorldPosition.x);
+            return true;
+        }
+
+        private bool TryGetClickedTile(Vector2 clickWorldPosition, out Tilemap clickedTilemap, out Vector3Int clickedCell)
+        {
+            clickedTilemap = null;
+            clickedCell = Vector3Int.zero;
+
+            if (groundTilemap != null && ContainsTileAtWorldPosition(groundTilemap, clickWorldPosition, out clickedCell))
+            {
+                clickedTilemap = groundTilemap;
+                return true;
+            }
+
+            Grid grid = groundTilemap != null ? groundTilemap.GetComponentInParent<Grid>() : FindAnyObjectByType<Grid>();
+            if (grid == null)
+            {
+                return false;
+            }
+
+            Tilemap[] tilemaps = grid.GetComponentsInChildren<Tilemap>();
+            for (int i = 0; i < tilemaps.Length; i++)
+            {
+                if (tilemaps[i] == groundTilemap)
+                {
+                    continue;
+                }
+
+                if (!ContainsTileAtWorldPosition(tilemaps[i], clickWorldPosition, out clickedCell))
+                {
+                    continue;
+                }
+
+                clickedTilemap = tilemaps[i];
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool ContainsTileAtWorldPosition(Tilemap tilemap, Vector2 worldPosition, out Vector3Int cellPosition)
+        {
+            cellPosition = Vector3Int.zero;
+            if (tilemap == null)
+            {
+                return false;
+            }
+
+            cellPosition = tilemap.WorldToCell(worldPosition);
+            return tilemap.HasTile(cellPosition);
+        }
+
+        private Tilemap FindFirstTerrainTilemap()
+        {
+            Tilemap[] tilemaps = FindObjectsByType<Tilemap>();
+            for (int i = 0; i < tilemaps.Length; i++)
+            {
+                Collider2D tilemapCollider = tilemaps[i].GetComponent<Collider2D>();
+                if (tilemapCollider != null && !IsOwnCollider(tilemapCollider) && IsInTerrainMask(tilemaps[i].gameObject.layer))
+                {
+                    return tilemaps[i];
+                }
+            }
+
+            return tilemaps.Length > 0 ? tilemaps[0] : null;
+        }
+
+        private Vector2 GetTileTopPoint(Tilemap tilemap, Vector3Int cellPosition, float clickWorldX)
+        {
+            Vector3 cellCenter = tilemap.GetCellCenterWorld(cellPosition);
+            Vector3 cellSize = tilemap.layoutGrid != null ? tilemap.layoutGrid.cellSize : Vector3.one;
+            return new Vector2(clickWorldX, cellCenter.y + cellSize.y * 0.5f);
+        }
+
+        private bool IsInTerrainMask(int layer)
+        {
+            return (terrainMask.value & (1 << layer)) != 0;
+        }
+
+        private bool IsTouchingGround(Vector2 position)
+        {
+            int hitCount = CastGroundSensor(position, groundCheckDistance);
+            return ContainsGroundHit(hitCount);
+        }
+
+        private bool TryGetGroundBelow(Vector2 position, float distance, out RaycastHit2D groundHit)
+        {
+            int hitCount = CastGroundSensor(position, distance);
+            for (int i = 0; i < hitCount; i++)
+            {
+                if (IsGroundHit(castHits[i]))
+                {
+                    groundHit = castHits[i];
+                    return true;
+                }
+            }
+
+            groundHit = default;
+            return false;
+        }
+
+        private int CastGroundSensor(Vector2 position, float distance)
+        {
+            return Physics2D.BoxCast(position + groundSensorOffset, groundSensorSize, 0f, Vector2.down, terrainFilter, castHits, distance);
+        }
+
+        private bool IsWallAhead(Vector2 position, float direction)
+        {
+            Vector2 sensorCenter = position + new Vector2(wallSensorOffset.x * direction, wallSensorOffset.y);
+            int hitCount = Physics2D.BoxCast(sensorCenter, wallSensorSize, 0f, Vector2.right * direction, terrainFilter, castHits, wallCheckDistance);
+            return ContainsWallHit(hitCount, direction);
+        }
+
+        private bool ContainsGroundHit(int hitCount)
+        {
+            for (int i = 0; i < hitCount; i++)
+            {
+                if (IsGroundHit(castHits[i]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool ContainsWallHit(int hitCount, float direction)
+        {
+            for (int i = 0; i < hitCount; i++)
+            {
+                if (IsWallHit(castHits[i], direction))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsGroundHit(RaycastHit2D hit)
+        {
+            return !IsOwnCollider(hit.collider) && hit.normal.y > 0.5f;
+        }
+
+        private bool IsWallHit(RaycastHit2D hit, float direction)
+        {
+            return !IsOwnCollider(hit.collider) && hit.normal.x * direction < -0.5f;
+        }
+
+        private bool IsVerticalBlockHit(RaycastHit2D hit, Vector2 direction)
+        {
+            if (IsOwnCollider(hit.collider))
+            {
+                return false;
+            }
+
+            return direction.y < 0f ? hit.normal.y > 0.5f : hit.normal.y < -0.5f;
+        }
+
+        private bool IsOwnCollider(Collider2D otherCollider)
+        {
+            return otherCollider == null || otherCollider.transform == transform;
+        }
+
+        private void StartJump()
+        {
+            if (!isGrounded)
             {
                 return;
             }
 
-            Vector3 position = transform.position;
-            float nextX = Mathf.MoveTowards(position.x, targetX, moveSpeed * Time.deltaTime);
-            transform.position = new Vector3(nextX, position.y, position.z);
+            verticalVelocity = Mathf.Sqrt(2f * gravity * jumpHeight);
+            isGrounded = false;
+        }
 
-            if (animator != null)
+        private void UpdateAnimation()
+        {
+            if (animator == null)
             {
-                float xVelocity = Mathf.Abs(nextX - position.x) / Time.deltaTime;
-                animator.SetFloat("xVelocity", xVelocity);
+                return;
             }
 
-            if (Mathf.Abs(transform.position.x - targetX) <= stopDistance)
-            {
-                transform.position = new Vector3(targetX, position.y, position.z);
-                hasTarget = false;
+            float xVelocity = hasTarget ? 1f : 0f;
+            float jumpBlend = verticalVelocity < 0f ? -1f : 0f;
+            bool shouldPulseLanding = landedThisFrame || landingPulseTimer > 0f;
 
-                if (animator != null)
-                {
-                    animator.SetFloat("xVelocity", 0f);
-                }
+            animator.SetFloat("xVelocity", Mathf.Abs(horizontalVelocity) > 0.01f ? xVelocity : 0f);
+            animator.SetFloat("Blend", jumpBlend);
+            animator.SetBool("isJumping", !isGrounded || isPreparingWallJump);
+            animator.SetBool("touchGround", shouldPulseLanding);
+
+            if (landingPulseTimer > 0f)
+            {
+                landingPulseTimer = Mathf.Max(0f, landingPulseTimer - Time.fixedDeltaTime);
             }
         }
     }
